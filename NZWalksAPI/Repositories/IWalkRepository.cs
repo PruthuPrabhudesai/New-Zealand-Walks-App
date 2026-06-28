aca7885a737842017a51304f64646fd0b2045540
Pruthu Prabhudesai <pruthu373@gmail.com>
2026-06-28 16:13:44 +0530

    Implemented Filtering, Sorting and Pagination

--- NZWalksAPI/Repositories/IWalkRepository.cs
+++ NZWalksAPI/Repositories/IWalkRepository.cs
@@ -6,7 +6,8 @@ namespace NZWalks.API.Repositories
     public interface IWalkRepository
     {
         Task<Walk> CreateAsync(Walk walk);
-        Task<List<Walk>> GetAllAsync();
+        Task<List<Walk>> GetAllAsync(string? filterOn = null, string? filterQuery = null,
+    string? sortBy = null, bool isAscending = true, int pageNumber = 1, int pageSize = 1000);
         Task<Walk?> GetByIdAsync(Guid id);
         Task<Walk> updateAsync(Guid id, Walk walkdomainmodel);
         Task<Walk?>DeleteAsync(Guid id);
